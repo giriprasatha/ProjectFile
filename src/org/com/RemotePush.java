e50168e436c0e3384096941a9faadfaf7bbe1833
giriprasatha <giriprasatharunagiri@gmail.com>
2021-11-08 12:54:38 +0530

    first commit

---
+++ src/org/com/RemotePush.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class RemotePush {
+	
+	private void name(){
+		System.out.println("jhajhsjhajhsjhaa");
+	}
+	private void mobileNo() {
+		System.out.println("937897892");
+	}
+	private void id() {
+		System.out.println("aa555");
+	}
+	
+}
